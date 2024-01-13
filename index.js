3131c89bec530c48a426b0ce7c187993750d8ea0
IceCYrip <karansable16@gmail.com>
2024-01-13 13:26:20 +0530

    CORs error resolved

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const port = 5000
 app.use(express.json())
 app.use(cors())
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization'
+  )
+  next()
+})
+
 // Available Routes
 
 app.get('/', (req, res) => {
